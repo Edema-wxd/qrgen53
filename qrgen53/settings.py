5f00235336b516a91de7edd673f2de968add167c
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-08-15 12:48:15 +0100

    static redirect

--- qrgen53/settings.py
+++ qrgen53/settings.py
@@ -146,7 +146,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
